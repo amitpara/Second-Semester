48a06e1199c210daa6c8a8e7cd8ce881c91fdad1
Amit Parajuli <33621030+amitpara@users.noreply.github.com>
2018-06-14 11:15:54 +0545

    Simple File

---
+++ HTML/First HTML file.cpp
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head> 
+		<title> First </title>
+	</head>
+	<body>
+		<!--comments go here -->
+		<h1> Second Semester </h1>
+		<h2> First HTML page </h2>
+		<p> Learning simple HTML programs with heading and paragraphs. <br> Other simple tags are used.
+		</p>
+	</body>
+</html>
